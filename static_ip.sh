b270e12aeb8309b7ea5df8fdf570f274e34b7885
vlaroque <arrageois@gmail.com>
2020-07-04 19:46:52 +0200

    lol

--- static_ip.sh
+++ static_ip.sh
@@ -1,8 +1,8 @@
 sed -i "s/allow-hotplug enp0s3/auto enp0s3/" /etc/network/interfaces
 sed -i "s/iface enp0s3 inet dhcp/iface enp0s3 inet static/" /etc/network/interfaces
 # inet 10.0.2.15/24 brd 10.0.2.255 gateway = 10.0.2.2
-if grep -xq "\taddress 10.0.2.3/30" "/etc/network/interfaces"
- then
+if grep -xq "address 10.0.2.3/30" "/etc/network/interfaces"
+then
 echo "\taddress 10.0.2.3/30" >> /etc/network/interfaces
 echo "\tgateway 10.0.2.2" >> /etc/network/interfaces
 fi
