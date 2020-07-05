095fbd4cd5ebccdd95cb853f0d38dd4a693b61c1
vlaroque <arrageois@gmail.com>
2020-07-05 14:12:05 +0200

    lo

--- static_ip.sh
+++ static_ip.sh
@@ -3,8 +3,10 @@ sed -i "s/iface enp0s3 inet dhcp/iface enp0s3 inet static/" /etc/network/interfa
 # inet 10.0.2.15/24 brd 10.0.2.255 gateway = 10.0.2.2
 if grep -q "10.0.2.3/30" /etc/network/interfaces
 then
-echo "\taddress 10.0.2.3/30" >> /etc/network/interfaces
-echo "\tgateway 10.0.2.2" >> /etc/network/interfaces
+	echo "already completed"
+else
+	echo "\taddress 10.0.2.3/30" >> /etc/network/interfaces
+	echo "\tgateway 10.0.2.2" >> /etc/network/interfaces
 fi
 systemctl restart networking
 echo finish
