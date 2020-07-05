c2e9c11c6121994bb3e03a8f3fea213ef7f786f2
vlaroque <arrageois@gmail.com>
2020-07-05 14:21:05 +0200

    nest

--- ssh_config.sh
+++ ssh_config.sh
@@ -3,7 +3,7 @@ apt install openssh-server
 sed -i "s/#Port 22/Port 2222/" /etc/network/interfaces
 echo "PermitRootLogin no" >> /etc/ssh/sshd_config
 echo "PasswordAuthentification no" >> /etc/ssh/sshd_config
-cp id_rsa.pub /home/user42/.ssh/
+cp id_rsa_deby.pub /home/user42/.ssh/
 touch /home/user42/.ssh/autorized_keys
 cat /home/user42/.ssh/id_rsa_deby.pub >> /home/user42/.ssh/autorized_keys
 service ssh restart
