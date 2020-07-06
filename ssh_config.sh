8b5f926c95f63f5ce349b95fc3b0f29936335fa5
vlaroque <arrageois@gmail.com>
2020-07-06 14:56:11 +0200

    nest

--- ssh_config.sh
+++ ssh_config.sh
@@ -3,9 +3,11 @@ if grep -q "Port 2222" /etc/ssh/sshd_config
 then
 	echo "ssh_config already completed"
 else
-	#echo "Port 2222" >> /etc/ssh/sshd_config
+	# Change default port to 2222
 	sed -i "s/#Port 22/Port 2222/" /etc/ssh/sshd_config
+	# Disable Root connextions
 	echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+	# Disable password connections. Only by public keys connection is available
 	echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
 	mkdir -p /home/user42/.ssh
 	cp id_rsa_deby.pub /home/user42/.ssh/
