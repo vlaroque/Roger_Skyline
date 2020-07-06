f02a05b3a127772dfcd6f473c4a6461a2c9807a1
vlaroque <arrageois@gmail.com>
2020-07-06 19:56:52 +0200

    nest

--- port_scan_protection.sh
+++ port_scan_protection.sh
@@ -1,1 +1,6 @@
 apt install portsentry
+sed -i "s/BLOCK_UDP="0"/BLOCK_UDP="1"/" /etc/portsentry/portsentry.conf
+sed -i "s/BLOCK_TDP="0"/BLOCK_TCP="1"/" /etc/portsentry/portsentry.conf
+sed -i "s/TCP_MODE="tcp"/TCP_MODE="atcp"/" /etc/default/portsentry
+sed -i "s/TCP_MODE="udp"/TCP_MODE="audp"/" /etc/default/portsentry
+service portsentry restart
