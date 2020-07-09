8931ebef6aca18d8fe915cb255c014b8b56976ea
vlaroque <arrageois@gmail.com>
2020-07-09 16:34:20 +0200

    nest

--- crontabMonitor.sh
+++ crontabMonitor.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 cat /var/log/syslog | grep crontab | grep RELOAD > new_crontab_change
+touch last_crontab_change new_crontab_change
 if diff last_crontab_change new_crontab_change then
 	echo lol
 else
